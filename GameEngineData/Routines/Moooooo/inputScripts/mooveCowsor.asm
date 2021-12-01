b7e9bc30cf2abc69a483f56bc87798db38f814e5
Kevin81 <kevin.van.der.burg@gmail.com>
2021-12-01 16:22:31 +0100

    Reverted demo menu screen

--- GameEngineData/Routines/Moooooo/inputScripts/mooveCowsor.asm
+++ GameEngineData/Routines/Moooooo/inputScripts/mooveCowsor.asm
@@ -1,18 +1,16 @@
-    RTS ; Remove me after demo
-
 ;; Get current Y_position of cursor 
-        LDX #1
-        LDA Object_y_hi, x
+	LDX #1
+	LDA Object_y_hi, x
 
 
 ;; Toggle position between #$60 and #$78
-        CMP #$60
-        BEQ +
-            LDA #$48
-        +
-        CLC
-        ADC #$18
-        STA Object_y_hi, x
+	CMP #$60
+	BEQ +
+		LDA #$48
+	+
+	CLC
+	ADC #$18
+	STA Object_y_hi, x
 
 
 ;; Return
--- GameEngineData/Routines/Moooooo/inputScripts/mooveCowsorToo.asm
+++ GameEngineData/Routines/Moooooo/inputScripts/mooveCowsorToo.asm
@@ -1,16 +1,16 @@
 ;; Get current Y_position of cursor	
-		LDX #1
-		LDA Object_y_hi, x
+	LDX #1
+	LDA Object_y_hi, x
 
 
 ;; Toggle position between #$60 and #$78
-		CMP #$90
-		BNE +
-			LDA #$50
-		+
-		CLC
-		ADC #$10
-		STA Object_y_hi, x
+	CMP #$90
+	BNE +
+		LDA #$50
+	+
+	CLC
+	ADC #$10
+	STA Object_y_hi, x
 
 
 ;; Return
